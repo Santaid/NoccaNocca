6c0195098bf1dd273658db0497cc932069e6579c
6320102 <youtairangyuan@gmail.com>
2022-10-05 02:50:52 +0900

    ゲームシステム完成 あとは細かい調整

--- Assets/Scripts/GameMainScript.cs
+++ Assets/Scripts/GameMainScript.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameMainScript : MonoBehaviour{
-	private int White=2;
-	private int Black=1;
-	private int Wall=15;
+	public bool End=false;
+	public int Turn;
+	public int White=2;
+	public int Black=1;
+	public int Wall=15;
 	[SerializeField] private int row = 5;
 	[SerializeField] private int line = 6;
 	[SerializeField] private GameObject player_cube_one;
 	[SerializeField] private GameObject player_cube_two;
 	[SerializeField] private GameObject click_place;
-	int[,] board_state; 
-	List<int[,]> former_boards = new List<int[,]>();
+	public int[,] board_state;
+	public int[,] board_top;
+	public List<int[,]> former_boards = new List<int[,]>();
 
 	public static GameMainScript instance;
 	public void Awake(){
@@ -23,19 +26,26 @@ public class GameMainScript : MonoBehaviour{
 
     // Start is called before the first frame update
     void Start(){
+		Turn=Black;
 		board_state = new int[row + 2, line + 2];
+		board_top=new int[row+2,line+2];
 		for(int j = 0; j < line + 2; j++){//壁を上下に作る
 			board_state[0,j] = Wall;
 			board_state[row + 1,j] = Wall;
+			board_top[0,j]=0;
+			board_top[row+1,j]=0;
 		}
 		for(int i = 1; i < row + 1; i ++){//0で埋める
 			for(int j = 0; j < line + 2 ; j++){
 				board_state[i, j] = 0;
+				board_top[i,j]=0;
 			}
 		}
 		for(int i = 1; i <= row; i++){//初期配置
 			board_state[i, 1] = Black;
 			board_state[i, line] = White;
+			board_top[i,1]=Black;
+			board_top[i,line]=White;
 			// Vector3(row方向, 高さ, line方向)
 			Vector3 pos_white = new Vector3(i, 1, line);
 			Vector3 pos_black = new Vector3(i, 1, 1);
@@ -48,33 +58,95 @@ public class GameMainScript : MonoBehaviour{
 		}
     }
 
-	int isEnd(){
-		// ゴールしたか
-		for(int i=1; i<=row; i++){
-			if(board_state[i,0]==Black){
-				return Black;
-			}else if(board_state[i,line+1]==White){
-				return White;
+	public void changeTurn(){
+		if(Turn==Black){
+			Turn=White;
+			Debug.Log("white turn");
+		}else{
+			Turn=Black;
+			Debug.Log("black turn");
+		}
+	}
+
+	public List<int[]> canMove(int r,int l){
+		List<int[]> res=new List<int[]>();
+		for(int i=-1; i<=1; i++){
+			for(int j=-1; j<=1; j++){
+				if(board_state[r+i,l+j]<=6 && !(i==0 && j==0)){
+					if(!(Turn==Black && l+j==0) && !(Turn==White && l+j==line+1)){
+						res.Add(new int[2] {r+i,l+j});
+					}
+				}
 			}
 		}
+		return res;
+	}
 
-		// 駒が動けないか
+	// move自体はルールによる移動制限はない
+	public void move(int from_x,int from_z,int to_x,int to_z){
+		int originalPos=board_state[from_x,from_z];
+		int movedPos=board_state[to_x,to_z];
+
+		// 移動元の処理
+		if(originalPos<=2){ //　一段
+			board_state[from_x,from_z]=0;
+			board_top[from_x,from_z]=0;
+		}else if(originalPos<=6){ // 二段
+			board_state[from_x,from_z]-=Turn<<1;
+			board_top[from_x,from_z]=board_state[from_x,from_z];
+		}else if(originalPos<15){ // 三段
+			board_state[from_x,from_z]-=Turn<<2;
+			if(board_state[from_x,from_z]<=4){ // 3or4->black
+				board_top[from_x,from_z]=Black;
+			}else{
+				board_top[from_x,from_z]=White;
+			}
+		}
+		// 移動先の処理
+		if(movedPos==0){ // 空白
+			board_state[to_x,to_z]+=Turn;
+		}else if(movedPos<=2){ // 一段
+			board_state[to_x,to_z]+=Turn<<1;
+		}else if(movedPos<=6){ // 二段
+			board_state[to_x,to_z]+=Turn<<2;
+		}
+		board_top[to_x,to_z]=Turn;
+		// Debug.Log("board : (from)"+board_state[from_x,from_z]+", (to)"+board_state[to_x,to_z]);
+		// Debug.Log("board_top : (from)"+board_top[from_x,from_z]+", (to)"+board_top[to_x,to_z]);
+	}
+
+	public int isEnd(){
 		bool white_top=true,black_top=true;
-		for(int i = 1; i <= row; i++){
-			for(int j = 1; j <= line; j++){
-				// 黒が上
-				if(board_state[i,j]==1 || board_state[i,j]==3 || board_state[i,j]==4 || board_state[i,j]>=7 || board_state[i,j]<=10){
+		// ゴールしたか
+		for(int i=1; i<=row; i++){
+			if(board_state[i,0]==White){
+				Debug.Log("White won");
+				End=true;
+				return Black;
+			}else if(board_state[i,line+1]==Black){
+				Debug.Log("Black won");
+				End=true;
+				return White;
+			}
+			// 駒が動けないか
+			for(int j=1; j<=line; j++){
+				if(board_top[i,j]==Black){
 					white_top=false;
-				}else if(board_state[i,j]!=0){ //白が上
+				}
+				if(board_top[i,j]==White){
 					black_top=false;
 				}
 			}
 		}
-		if(white_top){
-			Debug.Log("白の勝ち");
+		if(black_top && !white_top){
+			Debug.Log("Black won");
+			End=true;
+			return Black;
+		}
+		if(white_top && !black_top){
+			Debug.Log("White won");
+			End=true;
 			return White;
-		}else if(black_top){
-			Debug.Log("黒の勝ち");
 		}
 		return 0;
 	}
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -5,144 +5,153 @@ using System;
 
 public class PlayerMove : MonoBehaviour
 {
-    //public GameObject player;
-    private GameObject destination;
-    public bool PlayerIsSelected = false;
-    bool DestinationIsSelected = false;
-    void Update(){
-            if(Input.GetMouseButtonDown(0) && PlayerIsSelected == false && OtherIsSelected() == false && ChoosedAsDestination() == false){
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                RaycastHit hit;
-                if(Physics.Raycast(ray, out hit)){
-                    //need add other chess_is_jm
-                    if(hit.collider.gameObject == this.gameObject){
-                        //player = hit.collider.gameObject; 
-                        PlayerIsSelected = true;
-                        //string objName = player.name;
-                        Change_shader();
-                        Debug.Log("player " + this.gameObject.name);
-                        //Debug.Log(this.gameObject.transform.position);
-                
-                        //GetLocalPosition();
-                        //Debug.Log(PlayerIsSelected);
-                        //Debug.Log(DestinationIsSelected);
-                    }
-                }
-                //移動先の判定以外を無視する
-            }else if(Input.GetMouseButtonDown(0) && PlayerIsSelected == true && DestinationIsSelected == false && OtherIsSelected() == false){
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                RaycastHit hit;
-                if(Physics.Raycast(ray, out hit)){
-                    //need add other chess_is_jm
-                    if(hit.collider.gameObject == this.gameObject){
-                        //player = hit.collider.gameObject; 
-                        PlayerIsSelected = false;
-                        //string objName = player.name;
-                        Return_shader();
-                        Debug.Log("player " + this.gameObject.name + "canceled");
-                    }else if((hit.collider.gameObject.tag == "player_white" || hit.collider.gameObject.tag == "player_black" || hit.collider.gameObject.tag == "ClickPlace" ) && hit.collider.gameObject != this.gameObject){
-                        destination = hit.collider.gameObject;
-                        DestinationIsSelected = true;
-                        Debug.Log("destination " + destination.name);
-                        Move();
-                        Return_shader();   
-                    }
-                }
-                /*
-            }else if(Input.GetMouseButtonDown(1) && PlayerIsSelected == true && DestinationIsSelected == false){
-                //Debug.Log("1");
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                RaycastHit hit;
-                if(Physics.Raycast(ray, out hit) && (hit.collider.gameObject.tag == "player_white" || hit.collider.gameObject.tag == "player_black" || hit.collider.gameObject.tag == "ClickPlace" ) && hit.collider.gameObject != this.gameObject){
-                    destination = hit.collider.gameObject;
-                    DestinationIsSelected = true;
-                    Debug.Log("destination " + destination.name);
-                    Move();
-                    Return_shader();         
-                }
-                */
-            }
-    }
+	//public GameObject player;
+	private GameObject destination;
+	public bool PlayerIsSelected = false;
+	bool DestinationIsSelected = false;
+	private List<int[]> canPut;
+	void Update(){
+		if(GameMainScript.instance.End){
+			return;
+		}
 
-    #region Move
-    void Move(){
-        //二段目からの判定が変わる
-        Vector3 targetPosition;
-        if(destination.tag == "ClickPlace" ){
-            targetPosition = new Vector3(destination.transform.position.x, destination.transform.position.y  + 0.5f, destination.transform.position.z);
-        }else{
-            targetPosition = new Vector3(destination.transform.position.x, destination.transform.position.y  + destination.transform.localScale.y + 0.05f, destination.transform.position.z);
-            
-        }
-        
-        transform.position = targetPosition;
-        PlayerIsSelected = false;
-        DestinationIsSelected = false;
-    }
-    #endregion
+		if(Input.GetMouseButtonDown(0) && !PlayerIsSelected && !OtherIsSelected() && !ChoosedAsDestination() && isTurn()){
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if(Physics.Raycast(ray, out hit)){
+				//need add other chess_is_jm
+				if(hit.collider.gameObject == this.gameObject){
+					//player = hit.collider.gameObject; 
+					PlayerIsSelected = true;
+					//string objName = player.name;
+					Change_shader();
+					// Debug.Log("player " + this.gameObject.name);
+					//Debug.Log(this.gameObject.transform.position);	
+					//GetLocalPosition();
+					//Debug.Log(PlayerIsSelected);
+					//Debug.Log(DestinationIsSelected);
+				}
+			}
+		//移動先の判定以外を無視する
+		}else if(Input.GetMouseButtonDown(0) && PlayerIsSelected && !DestinationIsSelected && !OtherIsSelected()){
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if(Physics.Raycast(ray, out hit)){
+				//need add other chess_is_jm
+				if(hit.collider.gameObject == this.gameObject){
+					//player = hit.collider.gameObject; 
+					PlayerIsSelected = false;
+					//string objName = player.name;
+					Return_shader();
+					// Debug.Log("player " + this.gameObject.name + "canceled");
+				}else if((hit.collider.gameObject.tag == "player_white" || hit.collider.gameObject.tag == "player_black" || hit.collider.gameObject.tag == "ClickPlace" ) && hit.collider.gameObject != this.gameObject){
+					destination = hit.collider.gameObject;
+					// 移動先が合法かどうか
+					foreach (int[] item in canPut){
+						// Debug.Log("canput: "+item[0]+","+item[1]);
+						if(item[0]==(int)destination.transform.position.x && item[1]==(int)destination.transform.position.z){
+							DestinationIsSelected = true;
+							// Debug.Log("destination " + destination.name);
+							Move();
+							Return_shader();
+							GameMainScript.instance.changeTurn();
+							GameMainScript.instance.isEnd();
+							return;
+						}
+					}
+					PlayerIsSelected=false;
+					Return_shader();
+				}
+			}
+		}
+	}
 
-    #region Change_shader
-    public Material changed_shader;
-    void Change_shader(){
-        this.gameObject.GetComponent<MeshRenderer>().material = changed_shader;
-    }
-    #endregion
+	#region Move
+	void Move(){
+		//二段目からの判定が変わる
+		Vector3 targetPosition;
+		if(destination.tag == "ClickPlace" ){
+			targetPosition = new Vector3(destination.transform.position.x, destination.transform.position.y  + 0.5f, destination.transform.position.z);
+		}else{
+			targetPosition = new Vector3(destination.transform.position.x, destination.transform.position.y  + destination.transform.localScale.y + 0.05f, destination.transform.position.z);
+		}
+		// ボード情報の書き換え
+		GameMainScript.instance.move((int)transform.position.x,(int)transform.position.z,(int)destination.transform.position.x,(int)destination.transform.position.z);
+		transform.position = targetPosition;
+		PlayerIsSelected = false;
+		DestinationIsSelected = false;
+	}
+	#endregion
 
-    #region Return_shader
-    public Material returned_shader;
-    void Return_shader(){
-        this.gameObject.GetComponent<MeshRenderer>().material = returned_shader;
-    }
-    #endregion
+	#region Change_shader
+	public Material changed_shader;
+	void Change_shader(){
+		// 選択した駒を色変え
+		this.gameObject.GetComponent<MeshRenderer>().material = changed_shader;
+		// 動ける場所を色変え
+		canPut=GameMainScript.instance.canMove((int)this.transform.position.x,(int)this.transform.position.z);
+	}
+	#endregion
 
-    #region OtherIsSelected & ChoosedAsDestination
-    GameObject[] player_black_chesses;
-    GameObject[] player_white_chesses;
-    bool OtherIsSelected(){
-        player_black_chesses = GameObject.FindGameObjectsWithTag("player_black");
-        player_white_chesses = GameObject.FindGameObjectsWithTag("player_white");
-        for(int i = 0; i < player_black_chesses.Length; i++){
-            if(player_black_chesses[i] != this.gameObject && player_black_chesses[i].GetComponent<PlayerMove>().PlayerIsSelected == true){    
-                return true;
-            }
-            
-        }
-        for(int i = 0; i < player_white_chesses.Length; i++){
-            if(player_white_chesses[i] != this.gameObject && player_white_chesses[i].GetComponent<PlayerMove>().PlayerIsSelected == true){
-                return true;
-            }
-        }
-        return false;
-    }
+	#region Return_shader
+	public Material returned_shader;
+	void Return_shader(){
+		this.gameObject.GetComponent<MeshRenderer>().material = returned_shader;
+	}
+	#endregion
 
-    bool ChoosedAsDestination(){
-        player_black_chesses = GameObject.FindGameObjectsWithTag("player_black");
-        player_white_chesses = GameObject.FindGameObjectsWithTag("player_white");
-        for(int i = 0; i < player_black_chesses.Length; i++){
-            if(player_black_chesses[i] != this.gameObject && player_black_chesses[i].GetComponent<PlayerMove>().destination == this.gameObject){    
-                return true;
-            }
-            
-        }
-        for(int i = 0; i < player_white_chesses.Length; i++){
-            if(player_white_chesses[i] != this.gameObject && player_white_chesses[i].GetComponent<PlayerMove>().destination == this.gameObject){
-                return true;
-            }
-        }
-        return false;
-    }
-    #endregion
-    /*
-    #region Rule
-    void GetLocalPosition(){
-        private int localPosition;
-        localPosition = GameMain.GetComponent<GameMainScript>().board_state[this.gameObject.transform.position.x, this.gameObject.transform.position.z];
-        Debug.Log(localPosition);
-    }
-    
+	#region OtherIsSelected & ChoosedAsDestination
+	GameObject[] player_black_chesses;
+	GameObject[] player_white_chesses;
+	bool OtherIsSelected(){
+		player_black_chesses = GameObject.FindGameObjectsWithTag("player_black");
+		player_white_chesses = GameObject.FindGameObjectsWithTag("player_white");
+		for(int i = 0; i < player_black_chesses.Length; i++){
+			if(player_black_chesses[i] != this.gameObject && player_black_chesses[i].GetComponent<PlayerMove>().PlayerIsSelected == true){    
+				return true;
+			}
+			
+		}
+		for(int i = 0; i < player_white_chesses.Length; i++){
+			if(player_white_chesses[i] != this.gameObject && player_white_chesses[i].GetComponent<PlayerMove>().PlayerIsSelected == true){
+				return true;
+			}
+		}
+		return false;
+	}
 
-    #endregion
-*/
+	bool ChoosedAsDestination(){
+		player_black_chesses = GameObject.FindGameObjectsWithTag("player_black");
+		player_white_chesses = GameObject.FindGameObjectsWithTag("player_white");
+		for(int i = 0; i < player_black_chesses.Length; i++){
+			if(player_black_chesses[i] != this.gameObject && player_black_chesses[i].GetComponent<PlayerMove>().destination == this.gameObject){    
+				return true;
+			}
+			
+		}
+		for(int i = 0; i < player_white_chesses.Length; i++){
+			if(player_white_chesses[i] != this.gameObject && player_white_chesses[i].GetComponent<PlayerMove>().destination == this.gameObject){
+				return true;
+			}
+		}
+		return false;
+	}
+	#endregion
+
+	#region Rule
+	// 選択した駒がターンと一致するか
+	bool isTurn(){
+		int turn=0;
+		if(this.gameObject.tag=="player_black"){
+			turn=GameMainScript.instance.Black;
+		}
+		if(this.gameObject.tag=="player_white"){
+			turn=GameMainScript.instance.White;
+		}
+		return (turn==GameMainScript.instance.Turn);
+	}
+
+	#endregion
 }
 
 
