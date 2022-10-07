71c89286443a8f12c1b8c6590059eb51d6a370f4
6320102 <youtairangyuan@gmail.com>
2022-10-07 15:39:22 +0900

    ゲーム完成

--- Assets/Scripts/GameMainScript.cs
+++ Assets/Scripts/GameMainScript.cs
@@ -15,7 +15,8 @@ public class GameMainScript : MonoBehaviour{
 	[SerializeField] private GameObject click_place;
 	public int[,] board_state;
 	public int[,] board_top;
-	public List<int[,]> former_boards = new List<int[,]>();
+	public List<int[]> former_move = new List<int[]>();
+	public List<int[,]> former_boards=new List<int[,]>();
 
 	public static GameMainScript instance;
 	public void Awake(){
@@ -111,6 +112,8 @@ public class GameMainScript : MonoBehaviour{
 			board_state[to_x,to_z]+=Turn<<2;
 		}
 		board_top[to_x,to_z]=Turn;
+		// ボード情報を記録
+		former_move.Add(new int[]{from_x,from_z,to_x,to_z});
 		// Debug.Log("board : (from)"+board_state[from_x,from_z]+", (to)"+board_state[to_x,to_z]);
 		// Debug.Log("board_top : (from)"+board_top[from_x,from_z]+", (to)"+board_top[to_x,to_z]);
 	}
@@ -149,5 +152,39 @@ public class GameMainScript : MonoBehaviour{
 			return White;
 		}
 		return 0;
+	}
+
+	public void resetBoard(){
+		// 駒の削除
+		GameObject[] balckPieces=GameObject.FindGameObjectsWithTag("player_black");
+		GameObject[] whitePieces=GameObject.FindGameObjectsWithTag("player_white");
+		foreach(GameObject obj in balckPieces){
+			Destroy(obj);
+		}
+		foreach(GameObject obj in whitePieces){
+			Destroy(obj);
+		}
+
+		Turn=Black;
+		for(int i = 1; i <= row; i++){//初期配置
+			board_state[i, 1] = Black;
+			board_state[i, line] = White;
+			board_top[i,1]=Black;
+			board_top[i,line]=White;
+			// Vector3(row方向, 高さ, line方向)
+			Vector3 pos_white = new Vector3(i, 1, line);
+			Vector3 pos_black = new Vector3(i, 1, 1);
+			Instantiate(player_cube_one, pos_white, Quaternion.identity);
+			Instantiate(player_cube_two, pos_black, Quaternion.identity);
+		}
+	}
+
+	// 一手前を呼び出す
+	public void undo(){
+		changeTurn();
+		// リストの末尾要素を削除して末尾要素を呼び出す->一手前
+		former_move.RemoveAt(former_move.Count-1);
+		int[] index=former_move[former_move.Count-1];
+		move(index[2],index[3],index[0],index[1]);
 	}
 }
